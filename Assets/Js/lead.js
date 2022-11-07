2636c497bd79ec057830ad24d28f201c5a97cf41
Pankaj Sharma <442001ps@gmail.com>
2022-11-07 22:17:40 +0530

    Validation added

--- Assets/Js/lead.js
+++ Assets/Js/lead.js
@@ -8,6 +8,7 @@ const model_body = document.getElementById("model-body");
 const download1 = document.getElementById("Download1");
 const findout1 = document.getElementById("Find-Out1");
 const findout2 = document.getElementById("Find-Out2");
+var notifys = document.getElementById("notify");
 var isDownload = false;
 var once = false;
 
@@ -23,9 +24,21 @@ findout2.addEventListener("click", () => {
   isDownload = false;
 });
 
+// SHOW SUCCESS IN FORM
+const showSuccess = (message) => {
+  notifys.textContent = message;
+  notifys.className = "alert alert-success";
+};
+
+// SHOW ERROR IN FORM
+const ShowWarn = (message) => {
+  notifys.textContent = message;
+  notifys.className = "alert alert-danger";
+};
+
 const thankyou = () => {
   model_body.innerHTML =
-    '<div class="text-center"><img src="./Assets/Img/thankyou.gif" class="img-fluid" > <h5 class="fixed-bottom position-absolute" style="bottom:50px; font-weight:bold;padding:10px">We are soo glad that you connected with us We look forward to being a part of your career Our team will contact you soon for more details.</h5></div>';
+    '<div class="thankyou text-center"><img src="./Assets/Img/thankyou.gif" class="img-fluid" > <h5 class="" style=" font-weight:bold; padding:10px">We are soo glad that you connected with us We look forward to being a part of your career Our team will contact you soon for more details.</h5></div>';
 };
 
 const downloadFile = (url, fileName) => {
@@ -52,6 +65,36 @@ forms.addEventListener("submit", async (event) => {
   const Email = forms.elements["Email"];
   const Phone = forms.elements["Phone"];
   const City = forms.elements["City"];
+
+  if (_.isEmpty(FirstName.value)) {
+    return ShowWarn("First name cannot be empty");
+  }
+  if (_.isEmpty(LastName.value)) {
+    return ShowWarn("Last name cannot be empty");
+  }
+  if (_.isEmpty(Email.value)) {
+    return ShowWarn("Email cannot be empty");
+  }
+
+  if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(Email.value)) {
+    return ShowWarn("Please enter a valid Email");
+  }
+  if (_.isEmpty(Phone.value)) {
+    return ShowWarn("Phone number is required");
+  }
+  if (Phone.value.length > 10) {
+    return ShowWarn("Phone number must consist of 10 numbers");
+  }
+  if (Phone.value.length !== 10) {
+    return ShowWarn("Phone number must consist of 10 numbers");
+  }
+  if (!/^(?:\W*\d){10}\W*$/.test(Phone.value)) {
+    return ShowWarn("Phone number is invalid");
+  }
+
+  if (_.isEmpty(City.value)) {
+    return ShowWarn("City name is required");
+  }
 
   await fetch(scriptURL, {
     method: "POST",
